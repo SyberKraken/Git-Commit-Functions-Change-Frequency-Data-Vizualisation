90532b1810c4178ec99d5bec457344c7a4327a58
Simon Gradin <simgr011@student.liu.se>
2022-11-28 03:59:14 +0100

    custom data container added

---
+++ components/Menu.tsx
@@ -0,0 +1,16 @@
+export default function Menu(){
+
+    return(
+    <>
+    <div className="menuwrapper">
+        <button>weeee</button>
+        <h1>Treemap</h1>
+        <button>weeee</button>
+    </div>
+
+
+    
+    </>
+        
+    )
+}
